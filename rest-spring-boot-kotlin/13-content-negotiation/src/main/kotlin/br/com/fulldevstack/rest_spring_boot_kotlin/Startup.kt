0c9f7f7415bfd6ed36fcdb4587895dfbcf6b34c1
Alexander Alves <vendas77.rj@gmail.com>
2024-09-17 20:07:11 -0300

    Content Negotiation

---
+++ rest-spring-boot-kotlin/13-content-negotiation/src/main/kotlin/br/com/fulldevstack/rest_spring_boot_kotlin/Startup.kt
@@ -0,0 +1,10 @@
+package br.com.fulldevstack.rest_spring_boot_kotlin
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Startup
+
+fun main(args: Array<String>) {
+	runApplication<Startup>(*args)
+}
