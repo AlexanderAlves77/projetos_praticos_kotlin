372a36175f3ff35f066a7d45fcf5281bd1338685
Alexander Alves <vendas77.rj@gmail.com>
2024-09-17 11:28:03 -0300

    Understanding path params

---
+++ rest-spring-boot-kotlin/06-understanding-path-params/src/main/kotlin/br/com/fulldevstack/rest_spring_boot_kotlin/exceptions/UnsupportedMathOperationException.kt
@@ -0,0 +1,6 @@
+package br.com.fulldevstack.rest_spring_boot_kotlin.exceptions
+import java.lang.*
+
+class UnsupportedMathOperationException(exception: String?) : RuntimeException(exception) {
+
+}
