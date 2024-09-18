0c090d35b87f2f76e452d496bc7d15cdc8c920f2
Alexander Alves <vendas77.rj@gmail.com>
2024-09-18 09:13:43 -0300

    Implementing HATEOAS

---
+++ rest-spring-boot-kotlin/14-implementing-hateoas/src/main/kotlin/br/com/fulldevstack/rest_spring_boot_kotlin/data/vo/v1/PersonVO.kt
@@ -0,0 +1,14 @@
+package br.com.fulldevstack.rest_spring_boot_kotlin.data.vo.v1
+import com.github.dozermapper.core.Mapping
+import org.springframework.hateoas.RepresentationModel
+
+
+data class PersonVO(
+
+    @Mapping("id")
+    var key: Long = 0,
+    var firstName: String = "",
+    var lastName: String = "",
+    var address: String = "",
+    var gender: String = ""
+) : RepresentationModel<PersonVO>()
