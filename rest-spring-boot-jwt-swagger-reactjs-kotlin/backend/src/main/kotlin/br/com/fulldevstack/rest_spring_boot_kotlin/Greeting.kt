db9083985739937af40fdf0ab21ad77d99686f68
Alexander Alves <vendas77.rj@gmail.com>
2024-09-18 19:14:38 -0300

    Started the project

---
+++ rest-spring-boot-jwt-swagger-reactjs-kotlin/backend/src/main/kotlin/br/com/fulldevstack/rest_spring_boot_kotlin/Greeting.kt
@@ -0,0 +1,3 @@
+package br.com.fulldevstack.rest_spring_boot_kotlin
+
+class Greeting(val id: Long, val content: String)
---
+++ rest-spring-boot-jwt-swagger-reactjs-kotlin/backend/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,13 @@
+INSERT INTO person (id, first_name, last_name, address, gender) VALUES
+	(1, "Alexander", "Everton", "Nova Iguaçu - Rio de Janeiro", "Male"),
+	(2, "Kelvin", "Gustavo", "Nova Iguaçu - Rio de Janeiro", "Male"),
+	(3, "Sara", "Katherine", "Nova Iguaçu - Rio de Janeiro", "Female"),
+	(4, "Rita", "Wilemem", "Nova Iguaçu - Rio de Janeiro", "Female"),
+	(5, "Marly", "Alves", "Nova Iguaçu - Rio de Janeiro", "Female"),
+	(6, "José Carlos", "Alves", "Nova Iguaçu - Rio de Janeiro", "Male"),
+	(7, "Cíntia", "Carla", "Florania - Rio Grande do Norte", "Female"),
+	(8, "Thiago", "Rafael", "Cabo Frio - Rio de Janeiro", "Male"),
+	(9, "Jivago", "Iuri", "Madureira - Rio de Janeiro", "Male"),
+	(10, "Gleice", "Caroline", "Nova Iguaçu - Rio de Janeiro", "Female"),
+	(11, "Patrick", "Alves", "Cabo Frio - Rio de Janeiro", "Male"),
+	(12, "Joaquim", "Parente", "Nova Iguaçu - Rio de Janeiro", "Male");
