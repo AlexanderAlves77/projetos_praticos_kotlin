98caf9e0296451eac9715430940fcbd1c582a2b9
Alexander Alves <vendas77.rj@gmail.com>
2024-09-19 09:10:04 -0300

    Integrating Mysql Database API

--- rest-spring-boot-jwt-swagger-reactjs-kotlin/backend/src/main/kotlin/br/com/fulldevstack/rest_spring_boot_kotlin/controller/PersonController.kt
+++ rest-spring-boot-jwt-swagger-reactjs-kotlin/backend/src/main/kotlin/br/com/fulldevstack/rest_spring_boot_kotlin/controller/PersonController.kt
@@ -1,17 +1,18 @@
 package br.com.fulldevstack.rest_spring_boot_kotlin.controller
-import br.com.fulldevstack.rest_spring_boot_kotlin.converters.NumberConverter
-import br.com.fulldevstack.rest_spring_boot_kotlin.exceptions.UnsupportedMathOperationException
-import br.com.fulldevstack.rest_spring_boot_kotlin.math.SimpleMath
 import br.com.fulldevstack.rest_spring_boot_kotlin.model.Person
 import br.com.fulldevstack.rest_spring_boot_kotlin.services.PersonService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
-import java.util.concurrent.atomic.AtomicLong
 
 @RestController
 @RequestMapping("/person")
@@ -20,36 +21,33 @@ class PersonController {
     @Autowired
     private lateinit var service: PersonService
 
-    @RequestMapping(method = [RequestMethod.GET],
-        produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
     fun findAll(): List<Person> {
         return service.findAll()
     }
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.GET],
-        produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun findById(@PathVariable(value = "id") id: Long): Person {
         return service.findById(id)
     }
 
-    @RequestMapping(method = [RequestMethod.POST],
-        consumes = [MediaType.APPLICATION_JSON_VALUE],
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE])
     fun create(@RequestBody person: Person): Person {
         return service.create(person)
     }
 
-    @RequestMapping(method = [RequestMethod.PUT],
-        consumes = [MediaType.APPLICATION_JSON_VALUE],
+    @PutMapping(consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE])
     fun update(@RequestBody person: Person): Person {
         return service.update(person)
     }
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.DELETE],
+    @DeleteMapping(value = ["/{id}"],
         produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun delete(@PathVariable(value = "id") id: Long) {
+    fun delete(@PathVariable(value = "id") id: Long) : ResponseEntity<*> {
         service.delete(id)
+        return ResponseEntity.noContent().build<Any>()
     }
 
 }
--- rest-spring-boot-jwt-swagger-reactjs-kotlin/backend/src/main/kotlin/br/com/fulldevstack/rest_spring_boot_kotlin/services/PersonService.kt
+++ rest-spring-boot-jwt-swagger-reactjs-kotlin/backend/src/main/kotlin/br/com/fulldevstack/rest_spring_boot_kotlin/services/PersonService.kt
@@ -1,52 +1,51 @@
 package br.com.fulldevstack.rest_spring_boot_kotlin.services
+import br.com.fulldevstack.rest_spring_boot_kotlin.exceptions.ResourceNotFoundException
 import br.com.fulldevstack.rest_spring_boot_kotlin.model.Person
+import br.com.fulldevstack.rest_spring_boot_kotlin.repository.PersonRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.util.logging.Logger
-import java.util.concurrent.atomic.AtomicLong
+
 
 @Service
 class PersonService {
 
-    private val counter: AtomicLong = AtomicLong()
+    @Autowired
+    private lateinit var repository: PersonRepository
     private val logger = Logger.getLogger(PersonService::class.java.name)
 
     fun findAll() : List<Person> {
         logger.info("Finding All People!")
-
-        val persons : MutableList<Person> = ArrayList()
-        for (i in 0..7) {
-            val person = mockPerson(i)
-            persons.add(person)
-
-        }
-        return persons
+        return repository.findAll()
     }
 
     fun findById(id: Long) : Person {
         logger.info("Finding one person!")
-
-        val person = Person()
-        person.id = counter.incrementAndGet()
-        person.firstName = "Alexander"
-        person.lastName = "Alves"
-        person.address = "Nova Iguaçu - Rio de Janeiro - BR"
-        person.gender = "Male"
-        return person
+        return repository.findById(id)
+            .orElseThrow { ResourceNotFoundException("No records found for this ID!") }
     }
 
-    fun create(person: Person) = person
-
-    fun update(person: Person) = person
-
-    fun delete(id: Long) {}
-
-    private fun mockPerson(i: Int): Person {
-        val person = Person()
-        person.id = counter.incrementAndGet()
-        person.firstName = "Person Name $i"
-        person.lastName = "Last Name $i"
-        person.address = "Some Address in Brazil"
-        person.gender = "Male"
-        return person
+    fun create(person: Person) : Person {
+        logger.info("Creating one person with name ${person.firstName}!")
+        return repository.save(person)
     }
+
+    fun update(person: Person) : Person {
+        logger.info("Updating one person with ID ${person.id}!")
+        val entity = repository.findById(person.id)
+            .orElseThrow { ResourceNotFoundException("No records found for this ID!") }
+        entity.firstName = person.firstName
+        entity.lastName = person.lastName
+        entity.address = person.address
+        entity.gender = person.gender
+        return repository.save(entity)
+    }
+
+    fun delete(id: Long) {
+        logger.info("Deleting one person with ID $id!")
+        val entity = repository.findById(id)
+            .orElseThrow { ResourceNotFoundException("No records found for this ID!") }
+        repository.delete(entity)
+    }
+
 }
