6a1404abec371993902e6fb845a9797b2bb78fb7
Alexander Alves <vendas77.rj@gmail.com>
2024-09-21 10:20:22 -0300

    Criação do DTO e Entity Fornecedor

--- backend-kotlin-rest-api/src/main/kotlin/tssti/fullstack/backend_kotlin_rest_api/entity/Fornecedor.kt
+++ backend-kotlin-rest-api/src/main/kotlin/tssti/fullstack/backend_kotlin_rest_api/entity/Fornecedor.kt
@@ -3,11 +3,9 @@ package tssti.fullstack.backend_kotlin_rest_api.entity
 import jakarta.persistence.*
 
 @Entity
-data class Categoria(
-
+data class Fornecedor(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
+    val fornecedorID: Long? = null,
     var nome: String = "",
-    var unidade: String = ""
 )
